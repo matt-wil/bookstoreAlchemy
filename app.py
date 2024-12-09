8b4bf52863ef0a819e5750821a517c3f80781b83
Matt Williams <matthew.steven.williams@gmail.com>
2024-12-09 22:02:47 +0100

    create flask app and first route

--- app.py
+++ app.py
@@ -1,8 +1,57 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
+from dotenv import load_dotenv
+import logging
 
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.sqlite'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 
+db.init_app(app)
+logging.basicConfig(filename='flask_debug.log', level=logging.DEBUG)
+
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def authors():
+    if request.method == 'GET':
+        return render_template('add_author.html')
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        birth = request.form.get('birthdate')
+        death = request.form.get('date_of_death')
+
+        if not name:
+            flash("Name is required!", "error")
+            return render_template('add_author.html')
+
+        new_author = Author(name=name, birth=birth, death=death)
+        if new_author:
+            logging.debug('New Author Created')
+
+        try:
+            db.session.add(new_author)
+            db.session.commit()
+            flash(f"{new_author}", "success")
+            return redirect('/add_author')
+        except Exception as e:
+            db.session.rollback()
+            flash(f"An error occurred: {str(e)}", "error")
+            print(f"Error: {e}")
+            logging.debug(e)
+
+    return render_template('add_author.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# this only needs to be run once
+# with app.app_context():
+#     db.create_all()
---
+++ data_models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    birth = db.Column(db.String)
+    death = db.Column(db.String)
+
+    books = db.relationship('Book', back_populates='author')
+
+    def __repr__(self):
+        return f"Author(id={self.id}, name={self.name}\n, birth={self.birth}\ndeath={self.death}\n"
+
+    def __str__(self):
+        return f"Authors Details\nID: {self.id}\nName: {self.name}\nBirth: {self.birth}\nDeath: {self.death}\n"
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    isbn = db.Column(db.String(13))
+    title = db.Column(db.String)
+    publication_year = db.Column(db.Integer)
+
+    author = db.relationship('Author', back_populates='books')
+
+    def __repr__(self):
+        return f"Book(id={self.id}, author_id={self.author_id}, title={self.title}, publication_year={self.publication_year}, isbn={self.isbn}"
+
+    def __str__(self):
+        return f"Book Details\nID: {self.id}\nAuthor ID: {self.author_id}\nTitle: {self.title}\nPublished in {self.publication_year}\nISBN: {self.isbn}\n"
+
